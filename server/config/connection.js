fa574b012de1201bd7ad7ffb0c2f4c14fe767641
catxcoding <145952390+catxcoding@users.noreply.github.com>
2024-03-22 11:06:56 -0700

    updated Mongo URL

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://turbobaby:<o9WGdxEItnP94Mec>@cluster0.djrwp59.mongodb.net/');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://turbobaby:o9WGdxEItnP94Mec@cluster0.djrwp59.mongodb.net/');
 
 module.exports = mongoose.connection;
